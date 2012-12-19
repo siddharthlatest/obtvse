6c70411c5e2a4fbddcfee8cea53d060b8e1be0d4
Siddharth Kothari <sids.aquarius@gmail.com>
2012-12-19 10:23:42 +0530

    Use resonsive layout to fit content on mobile screens

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ Obtvse::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( bootstrap.css )
+  config.assets.precompile += %w( bootstrap-responsive.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
